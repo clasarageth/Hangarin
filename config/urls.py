f1bcf614244e74b1bb6b28d0eb6d066df14e096f
clasarageth <getheljayce.clasara@gmail.com>
2026-03-12 11:02:00 +0800

    subtasks listview

--- config/urls.py
+++ config/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from tasks import views
 from django.contrib import admin
 from django.urls import path
-from tasks.views import TaskListView, TaskCreateView, TaskUpdateView, SubTaskListView
+from tasks.views import TaskListView, TaskCreateView, TaskUpdateView, SubTaskListView, SubTaskUpdateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TaskListView.as_view(), name='task-list'),
     path('task_list/add', TaskCreateView.as_view(), name='task-add'),
     path('task_list/<pk>', TaskUpdateView.as_view(), name='task-update'),
     path('subtasks/', SubTaskListView.as_view(), name='subtask-list'),
+    path('subtasks/<pk>', SubTaskUpdateView.as_view(), name='subtask-update'),
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task, SubTask
-from .forms import TaskForm
+from .forms import TaskForm, SubTaskForm
 
 # Create your views here.
 
@@ -31,3 +31,9 @@ class SubTaskListView(ListView):
     model = SubTask
     template_name = 'subtasks_list.html'
     context_object_name = 'subtasks'
+
+class SubTaskUpdateView(UpdateView):
+    model = SubTask
+    form_class = SubTaskForm
+    template_name = 'subtasks_form.html'
+    success_url = reverse_lazy('subtask-list')
